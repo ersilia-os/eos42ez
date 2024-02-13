ba6d2407279db09341bde06f779f944f7cbf8a96
Richiio-portfolio <chiorlusarima@gmail.com>
2024-02-13 12:31:34 +0100

    local test

--- model/framework/code/main.py
+++ model/framework/code/main.py
@@ -1,9 +1,8 @@
-# imports
 import os
 import csv
 import sys
-from rdkit import Chem
-from rdkit.Chem.Descriptors import MolWt
+
+import chemprop
 
 # parse arguments
 input_file = sys.argv[1]
@@ -11,29 +10,72 @@ output_file = sys.argv[2]
 
 # current file directory
 root = os.path.dirname(os.path.abspath(__file__))
+hepg_model = os.path.abspath(os.path.join(root, "..", "..", "checkpoints", "cytotox_hepg2"))
+HSK_model = os.path.abspath(os.path.join(root, "..", "..", "checkpoints", "cytotox_primary"))
+IMR_model = os.path.abspath(os.path.join(root, "..", "..", "checkpoints", "cytotox_imr90"))
 
-# my model
-def my_model(smiles_list):
-    return [MolWt(Chem.MolFromSmiles(smi)) for smi in smiles_list]
+def my_model1(smiles_list):
+    
+    smiles_list_list= [[smiles] for smiles in smiles_list]  
+    arguments = [
+    '--test_path', '/dev/null',
+    '--preds_path', '/dev/null',
+    '--checkpoint_dir', hepg_model,
+    '--features_generator', 'rdkit_2d_normalized',
+    '--no_features_scaling'
+    ]
 
+    args = chemprop.args.PredictArgs().parse_args(arguments)
+    preds = chemprop.train.make_predictions(args=args, smiles=smiles_list_list)
+    return preds
 
-# read SMILES from .csv file, assuming one column with header
+def my_model2(smiles_list):
+    
+    smiles_list_list= [[smiles] for smiles in smiles_list]  
+    arguments = [
+    '--test_path', '/dev/null',
+    '--preds_path', '/dev/null',
+    '--checkpoint_dir', HSK_model,
+    '--features_generator', 'rdkit_2d_normalized',
+    '--no_features_scaling'
+    ]
+
+    args = chemprop.args.PredictArgs().parse_args(arguments)
+    preds = chemprop.train.make_predictions(args=args, smiles=smiles_list_list)
+    return preds
+
+def my_model3(smiles_list):
+    
+    smiles_list_list= [[smiles] for smiles in smiles_list]  
+    arguments = [
+    '--test_path', '/dev/null',
+    '--preds_path', '/dev/null',
+    '--checkpoint_dir', IMR_model,
+    '--features_generator', 'rdkit_2d_normalized',
+    '--no_features_scaling'
+    ]
+
+    args = chemprop.args.PredictArgs().parse_args(arguments)
+    preds = chemprop.train.make_predictions(args=args, smiles=smiles_list_list)
+    return preds
+
+# Function to extract desired values from model predictions
+def extract_values(predictions):
+    return [str(prediction[0]) for prediction in predictions]
+
+# Read SMILES from .csv file
 with open(input_file, "r") as f:
     reader = csv.reader(f)
     next(reader)  # skip header
     smiles_list = [r[0] for r in reader]
 
-# run model
-outputs = my_model(smiles_list)
+# Run models and extract desired values
+outputs1 = extract_values(my_model1(smiles_list))
+outputs2 = extract_values(my_model2(smiles_list))
+outputs3 = extract_values(my_model3(smiles_list))
 
-#check input and output have the same lenght
-input_len = len(smiles_list)
-output_len = len(outputs)
-assert input_len == output_len
-
-# write output in a .csv file
 with open(output_file, "w") as f:
     writer = csv.writer(f)
-    writer.writerow(["value"])  # header
-    for o in outputs:
-        writer.writerow([o])
+    writer.writerow(["HepG2", "HSkMC", "IMR_90"])  # header with column names
+    for o1, o2, o3 in zip(outputs1, outputs2, outputs3):
+        writer.writerow([o1, o2, o3])
