a377f7e2ee7048fc51e08fe8a2b3be7002186761
Miquel Duran-Frigola <miquelduranfrigola@gmail.com>
2025-01-05 08:37:52 +0100

    added columns

--- model/framework/code/main.py
+++ model/framework/code/main.py
@@ -76,6 +76,6 @@ outputs3 = extract_values(my_model3(smiles_list))
 
 with open(output_file, "w") as f:
     writer = csv.writer(f)
-    writer.writerow(["HepG2", "HSkMC", "IMR_90"])  # header with column names
+    writer.writerow(["cytotoxicity_hepg2", "cytotoxicity_hskmc", "cytotoxicity_imr90"])  # header with column names
     for o1, o2, o3 in zip(outputs1, outputs2, outputs3):
         writer.writerow([o1, o2, o3])
